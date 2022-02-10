911d371654ddda0242782a6aec3f54f87fef3440
teuncm <teun.c.mathijssen@gmail.com>
2022-02-10 17:26:00 +0100

    Commit flask structure

---
+++ swagger_server/controllers/default_controller.py
@@ -0,0 +1,48 @@
+import connexion
+import six
+
+from swagger_server.models.student import Student  # noqa: E501
+from swagger_server import util
+
+from swagger_server.service.student_service import *
+
+def add_student(body=None):  # noqa: E501
+    """adds student
+
+    Adds a student to the system  # noqa: E501
+
+    :param body: Student item to add
+    :type body: dict | bytes
+
+    :rtype: float
+    """
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+        return add(body)
+    return 500, 'error'
+
+
+def delete_student(student_id):  # noqa: E501
+    """deletes student
+
+    Deletes a single student  # noqa: E501
+
+    :param student_id: the uid
+    :type student_id: float
+
+    :rtype: None
+    """
+    return delete(student_id)
+
+
+def get_student_by_id(student_id):  # noqa: E501
+    """gets student
+
+    Returns a single student  # noqa: E501
+
+    :param student_id: the uid
+    :type student_id: float
+
+    :rtype: Student
+    """
+    return get_by_id(student_id)
